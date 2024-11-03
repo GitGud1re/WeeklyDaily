9e8adfdf68a5e7e4bdf1806796c8c47e414b894d
GitGud1Re <www.moon2040@gmail.com>
2024-11-02 22:07:41 -0700

    fixed mobile js for background

--- JsBG.js
+++ JsBG.js
@@ -6,10 +6,13 @@ function scaleBackgroundImage() {
         body.style.backgroundSize = 'contain'; // Scale background for mobile
         body.style.backgroundRepeat = 'no-repeat'; // Prevent repeating
         body.style.backgroundAttachment = 'fixed'; // Fixed attachment for mobile
+        body.style.backgroundPosition = 'center'; // Center the image
+        body.style.backgroundImage = 'rotate(90deg)'; // Rotate background for mobile if needed
     } else {
         body.style.backgroundSize = 'cover'; // Scale background for PC
         body.style.backgroundRepeat = 'no-repeat'; // Prevent repeating
         body.style.backgroundAttachment = 'fixed'; // Fixed attachment for PC, preventing scrolling
+        body.style.backgroundPosition = 'center'; // Center the image for PC too
     }
 }
 
